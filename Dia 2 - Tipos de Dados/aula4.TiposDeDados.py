1c09f5cd20451dc8a07d3a89dda99be6905a7356
Kristiansen <120206974+Kristin4ns3n@users.noreply.github.com>
2024-06-13 13:41:54 -0300

    Add files via upload
    Tipos de dados e typescasting

---
+++ Dia 2 - Tipos de Dados/aula4.TiposDeDados.py
@@ -0,0 +1,37 @@
+# Tipos int e float 
+# int -> Número inteiro
+# O tipo int representa qualquer número positvo ou negativo. Um int sem sinal é considerado positivo
+
+#Tipos primitivos 
+def TiposDeDados():
+    A = 1
+    B = 2.2
+    C = "Ana"
+    D = True
+    E = []                                                      # E is a list instead of an array
+    F = {'Cristina': '23456789011'}                             # Wow! This is a dict -> dictionary. Is always a pair/set
+    G = (['elephant', 'giraffe', 'monkey', 'tiger', 'lion'])    # This is a LIST
+    H = {'elephant', 'giraffe', 'monkey', 'tiger', 'lion'}      # This is a SET
+
+    print('\n')
+    print(type(A))                                              # type() é uma classe
+    print(type(B))
+    print(type(C))
+    print(type(D))
+    print(type(E))      
+    print(type(F))      
+    print(type(G))
+    print(type(H))
+
+TiposDeDados()
+
+# float -> Número com ponto flutuante 
+# O tipo float representa qualquer número positivo ou negativo com ponto flutuante 
+# O float sem sinal é considerado positivo
+
+# A função type mostra o tipo que o Python inferiu o valor  
+
+
+# Tipo de Dado bool (Boolean)
+# Ao questionar algo em um programa só existem duas respostas possíveis: True ou False 
+
